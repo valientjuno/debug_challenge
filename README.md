490b8c3246d9836bd91be1787c73ed17dc12e393
valientjuno <jessedoake91@gmail.com>
2025-06-23 09:19:24 -0600

    partial

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # debug_challenge
 
+This is a challenge project from module three of my course studys.
---
+++ script.js
@@ -0,0 +1,14 @@
+// make sure these match the HTML element names for the ID then class.
+let input = document.querySelector(".name");
+let display = document.querySelector(".display");
+
+// This code should be working
+input.addEventListener("input", function () {
+//   const name = input.value;
+//   display.textContent = name;
+// });
+
+// missing function?
+
+// you should see 5 in the output of your console
+console.log(simpleMath(2, 3));
