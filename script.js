d915079017a9c9a454139cb00b67a7624e987f22
valientjuno <jessedoake91@gmail.com>
2025-06-23 12:44:30 -0600

    js update

--- script.js
+++ script.js
@@ -3,16 +3,15 @@ let input = document.querySelector(".name");
 let display = document.querySelector(".display");
 
 // This code should be working
-input.addEventListener("input", function()){
-};
-//   const name = input.value;
-//   display.textContent = name;
-// });
+input.addEventListener("input", function () {
+  const name = input.value;
+  display.textContent = name;
+});
 
 // missing function?
 
 // you should see 5 in the output of your console
-const addNums = (num1 = 2, num2 = 3) =>{
-    console.log(num1+num2);
-}
-addNums (2, 3);
+const addNums = (num1 = 2, num2 = 3) => {
+  console.log(num1 + num2);
+};
+addNums(2, 3);
