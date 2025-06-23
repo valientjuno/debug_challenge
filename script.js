d58b99db4aa352c5ff4d8a113dbc663ec8e6acc5
valientjuno <jessedoake91@gmail.com>
2025-06-23 09:50:57 -0600

    another one

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let input = document.querySelector(".name");
 let display = document.querySelector(".display");
 
 // This code should be working
-input.addEventListener("input", function () {
+input.addEventListener("input", function (add) );
 //   const name = input.value;
 //   display.textContent = name;
 // });
