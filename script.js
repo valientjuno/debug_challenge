15f775a61941c32bd37b120ee0709b04806152c1
valientjuno <jessedoake91@gmail.com>
2025-06-23 12:58:13 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let input = document.querySelector(".name");
 let display = document.querySelector(".display");
 
 // This code should be working
-input.addEventListener("input", function () {
-  const name = input.value;
-  display.textContent = name;
-});
+// input.addEventListener("input", function () {
+//   const name = input.value;
+//   display.textContent = name;
+// });
 
 // missing function?
 
