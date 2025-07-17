a5b0901c708a2f98b4411d773e3f86b8d3d296d0
valientjuno <jessedoake91@gmail.com>
2025-07-17 08:46:42 -0600

    not getting it

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 // make sure these match the HTML element names for the ID then class.
+
 let input = document.querySelector(".name");
 let display = document.querySelector(".display");
 
-console.log(input, display)
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e){
+    e.preventDefault();
+}
+
+
 
 // This code should be working
 input.addEventListener("input", function () {
@@ -12,5 +19,9 @@ input.addEventListener("input", function () {
 
 // missing function?
 
+
+
+
 // you should see 5 in the output of your console
-console.log(simpleMath(2, 3));
+const addNums = (num1=2, num2=3)=> num1 + num2;
+console.log(num1 + num2);
